17d49c09a8b6a53297720ce9bd3dc31fcc195414
Ben Gamari <ben@smart-cactus.org>
2016-06-21 01:32:53 +0200

    Matching on job state

--- TPar/JobMatch.hs
+++ TPar/JobMatch.hs
@@ -5,8 +5,10 @@ module TPar.JobMatch where
 import Data.Foldable (traverse_)
 import Control.Monad (void)
 import Control.Applicative ((<|>))
-import Data.Binary
+import System.Exit
 import GHC.Generics
+
+import Data.Binary
 import Text.Trifecta
 
 import TPar.Types
@@ -46,9 +48,19 @@ data JobMatch = NoMatch
               | NameMatch Glob
               | JobIdMatch JobId
               | AltMatch [JobMatch]
+              | StateMatch StateMatch
               deriving (Generic, Show)
 
 instance Binary JobMatch
+
+data StateMatch = IsQueued
+                | IsRunning
+                | IsFinished (Maybe ExitCode)
+                | IsFailed
+                | IsKilled
+                deriving (Generic, Show)
+
+instance Binary StateMatch
 
 jobMatches :: JobMatch -> Job -> Bool
 jobMatches NoMatch            _   = False
@@ -57,10 +69,20 @@ jobMatches (NameMatch glob)   job = globMatches glob name
   where JobName name = jobName $ jobRequest job
 jobMatches (JobIdMatch jobid) job = jobId job == jobid
 jobMatches (AltMatch alts)    job = any (`jobMatches` job) alts
+jobMatches (StateMatch s)     job = stateMatches s (jobState job)
+
+stateMatches :: StateMatch -> JobState -> Bool
+stateMatches IsQueued                 Queued           = True
+stateMatches IsRunning                (Running _)      = True
+stateMatches (IsFinished Nothing)     (Finished _)     = True
+stateMatches (IsFinished (Just code)) (Finished code') = code == code'
+stateMatches IsFailed                 (Failed _)       = True
+stateMatches IsKilled                 Killed           = True
+stateMatches _                        _                = False
 
 parseJobMatch :: Parser JobMatch
 parseJobMatch =
-    AltMatch <$> (negMatch <|> nameMatch <|> jobIdMatch) `sepBy1` char ','
+    AltMatch <$> (negMatch <|> nameMatch <|> jobIdMatch <|> stateMatch) `sepBy1` char ','
   where
     allMatch :: Parser JobMatch
     allMatch = char '*' >> pure AllMatch
@@ -70,6 +92,11 @@ parseJobMatch =
         string "name="
         NameMatch <$> between (char '"') (char '"') parseGlob
 
+    stateMatch :: Parser JobMatch
+    stateMatch = do
+        string "state="
+        StateMatch <$> parseJobState
+
     jobIdMatch :: Parser JobMatch
     jobIdMatch = do
         string "id="
@@ -78,3 +105,20 @@ parseJobMatch =
     negMatch = do
         char '!'
         NegMatch <$> between (char '(') (char ')') parseJobMatch
+
+parseJobState :: Parser StateMatch
+parseJobState =
+        (string "queued"    *> pure IsQueued)
+    <|> (string "running"   *> pure IsRunning)
+    <|> (string "finished"  *> pure (IsFinished Nothing))
+    <|> (string "done"      *> pure (IsFinished Nothing))
+    <|> (string "succeeded" *> pure (IsFinished (Just ExitSuccess)))
+    <|> (string "code="     *> (IsFinished . Just <$> exitCode))
+    <|> (string "failed"    *> pure IsFailed)
+    <|> (string "killed"    *> pure IsKilled)
+  where
+    exitCode = (? "exit code") $ do
+      n <- integer
+      pure $ case n of
+               0 -> ExitSuccess
+               _ -> ExitFailure (fromIntegral n)
